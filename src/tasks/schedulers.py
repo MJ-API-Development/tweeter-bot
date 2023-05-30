25f51acaafd8ea05b350bf5a41637a9dcbc43ffe
freelancing-solutions <mobiusndou@gmail.com>
2023-05-30 13:49:19 +0200

    Added Boiler Plate for Task Scheduler

--- src/tasks/schedulers.py
+++ src/tasks/schedulers.py
@@ -3,5 +3,13 @@ class TaskScheduler:
     def __init__(self):
         pass
 
+    async def compose_articles(self):
+        """
+            **compose_articles**
+                create a list of articles that can be used to send tweets
+        :return:
+        """
+        pass
+
     async def run(self):
         pass
